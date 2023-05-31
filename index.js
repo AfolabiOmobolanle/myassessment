e4b8737faae11be6937907edef67f7c03d0a5d22
Bhee00 <afolabiomobolanle00@gmail.com>
2023-05-31 15:40:21 +0100

    my test commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let email = document.querySelector("#email");
 let country= document.querySelector("#country")
 let state= document.querySelector("#state")
 let city = document.querySelector("#city")
+let userImage = document.querySelector("#userImage")
 
 
 const key =`https://randomuser.me/api`;
@@ -16,7 +17,7 @@ function getProfile () {
         return response.json()
     }).then(data =>{
         console.log(data.results[0]);
-    
+        fullname.textContent = data.results[0].name.first + " " + data.results[0].name.last
         title.textContent =data.results[0].name.title
         phone.textContent =data.results[0].phone
         email.textContent =data.results[0].email
@@ -24,7 +25,7 @@ function getProfile () {
         state.textContent =data.results[0].location.state
         city.textContent =data.results[0].location.city
 
-
+userImage.src = data.results[0].picture.medium
 
        
     })
